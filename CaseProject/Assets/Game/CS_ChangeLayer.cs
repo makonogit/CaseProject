e4e96004e64087f0881b1d7fc5bb106f3dd46c81
KanMako <makosan5103@gmail.com>
2024-04-18 16:47:19 +0900

    Update:ステージデータとレイヤー管理のスクリプト更新

--- CaseProject/Assets/Game/CS_ChangeLayer.cs
+++ CaseProject/Assets/Game/CS_ChangeLayer.cs
@@ -19,16 +19,26 @@ public class CS_ChangeLayer : MonoBehaviour
         public Vector3 Pos;
         //[SerializeField]
         public GameObject LayerObj;
+
+        public LayerData(Vector3 scale,Vector3 pos,GameObject obj)
+        {
+            Scale = scale;
+            Pos = pos;
+            LayerObj = obj;
+        }
+
     }
 
     [SerializeField, Header("レイヤー情報")]
-    private LayerData[] m_Layer = new LayerData[3];
+    private List<LayerData> m_Layer = new List<LayerData>();
 
-    private int m_nOldLayer = 1;    //前のレイヤー
-    private int m_nNowLayer = 1;    //現在のレイヤー
+    private int m_nOldLayer = 2;    //前のレイヤー
+    private int m_nNowLayer = 2;    //現在のレイヤー
+
+    private const int m_MoveMax = 3;   //最大レイヤー数
 
     [SerializeField, Header("手のデータスクリプト")]
-    private CS_HandPoseData m_handposedata;
+    private CS_HandSigns m_handsigns;
 
     private bool m_isPush = false;      //押し引きの判定
 
@@ -36,112 +46,76 @@ public class CS_ChangeLayer : MonoBehaviour
 
     void Update()
     {
-        int pushdata = m_handposedata.PUSHDATA;
+        
+        int pushdata = 1;
 
-        if ((pushdata == 1 || pushdata == 0) && !m_isPush)
+        if(!m_isPush) pushdata = m_handsigns.PushHand();
+
+        if ((pushdata == 1 && m_nNowLayer < m_MoveMax) || 
+            (pushdata == 0 && m_nNowLayer > 1))
         {
             m_isPush = true;
 
             //更新前のレイヤーを保存
             m_nOldLayer = m_nNowLayer;
+            Debug.Log(pushdata);
 
             //現在のレイヤーを更新
-            m_nNowLayer = pushdata == 1 ? m_nNowLayer + 1 : m_nNowLayer - 1;
+            if(pushdata == 1)
+            {
+                m_nNowLayer++;
+                for (int i = m_Layer.Count - 1; i < 1; i++)
+                {
+                    Debug.Log("I" + i);
+                    m_Layer[i] = new LayerData(m_Layer[i].Scale, m_Layer[i].Pos, m_Layer[i - 1].LayerObj);
+                }
+            }
+            if(pushdata == 0) 
+            {
+                m_nNowLayer--;
+                for (int i = 0; i < m_Layer.Count - 1; i++)
+                {
+                    m_Layer[i] = new LayerData(m_Layer[i].Scale, m_Layer[i].Pos, m_Layer[i + 1].LayerObj);
+                }
+            }
 
             //レイヤーの移動制限
-            if (m_nNowLayer > 2) { m_nNowLayer = 2; }
-            if (m_nNowLayer < 0) { m_nNowLayer = 0; }
+            //if (m_nNowLayer > m_LayerMax) { m_nNowLayer = m_LayerMax; }
+            //if (m_nNowLayer < 0) { m_nNowLayer = 0; }
+
         }
 
 
-        if (m_isPush)
+        if (LayerChange())
         {
-            m_fTimer += Time.deltaTime;
-
-            //レイヤーが更新されていたら
-            if (m_nOldLayer < m_nNowLayer)    //押し
-            {
-                if (m_nNowLayer == 2)
-                {
-                    //奥のレイヤーを非アクティブにする
-                    m_Layer[m_nNowLayer].LayerObj.SetActive(false);
-
-                    //レイヤーを１つ後ろにする
-                    m_Layer[m_nOldLayer - 1].LayerObj.transform.localScale =
-                        Vector3.Lerp(m_Layer[m_nOldLayer - 1].LayerObj.transform.localScale, m_Layer[m_nOldLayer].Scale, Time.deltaTime);
-                    m_Layer[m_nOldLayer - 1].LayerObj.transform.position =
-                        Vector3.Lerp(m_Layer[m_nOldLayer - 1].LayerObj.transform.position, m_Layer[m_nOldLayer].Pos, Time.deltaTime);
-
-                    m_Layer[m_nOldLayer].LayerObj.transform.localScale =
-                        Vector3.Lerp(m_Layer[m_nOldLayer].LayerObj.transform.localScale, m_Layer[m_nNowLayer].Scale, Time.deltaTime);
-                    m_Layer[m_nOldLayer].LayerObj.transform.position =
-                        Vector3.Lerp(m_Layer[m_nOldLayer].LayerObj.transform.position, m_Layer[m_nNowLayer].Pos, Time.deltaTime);
-
-                }
-
-                if (m_nNowLayer == 1)
-                {
-                    //手前のレイヤーをアクティブにする
-                    m_Layer[m_nNowLayer - 1].LayerObj.SetActive(true);
-
-                    //レイヤーを１つ後ろにする
-                    m_Layer[m_nNowLayer].LayerObj.transform.localScale =
-                        Vector3.Lerp(m_Layer[m_nNowLayer].LayerObj.transform.localScale, m_Layer[m_nOldLayer + 1].Scale, Time.deltaTime);
-                    m_Layer[m_nNowLayer].LayerObj.transform.position =
-                        Vector3.Lerp(m_Layer[m_nNowLayer].LayerObj.transform.position, m_Layer[m_nOldLayer + 1].Pos, Time.deltaTime);
-
-                    m_Layer[m_nNowLayer + 1].LayerObj.transform.localScale =
-                        Vector3.Lerp(m_Layer[m_nNowLayer + 1].LayerObj.transform.localScale, m_Layer[m_nOldLayer + 2].Scale, Time.deltaTime);
-                    m_Layer[m_nNowLayer + 1].LayerObj.transform.position =
-                        Vector3.Lerp(m_Layer[m_nNowLayer + 1].LayerObj.transform.position, m_Layer[m_nOldLayer + 2].Pos, Time.deltaTime);
-                }
-
-            }
-
-            if (m_nOldLayer > m_nNowLayer)    //引き
-            {
-                if (m_nNowLayer == 0)
-                {
-                    //手前のレイヤーを非アクティブにする
-                    m_Layer[m_nNowLayer].LayerObj.SetActive(false);
-
-                    //レイヤーを１つ前にする
-                    m_Layer[m_nOldLayer + 1].LayerObj.transform.localScale =
-                        Vector3.Lerp(m_Layer[m_nOldLayer + 1].LayerObj.transform.localScale, m_Layer[m_nOldLayer].Scale, Time.deltaTime);
-                    m_Layer[m_nOldLayer + 1].LayerObj.transform.position =
-                        Vector3.Lerp(m_Layer[m_nOldLayer + 1].LayerObj.transform.position, m_Layer[m_nOldLayer].Pos, Time.deltaTime);
-
-                    m_Layer[m_nOldLayer].LayerObj.transform.localScale =
-                        Vector3.Lerp(m_Layer[m_nOldLayer].LayerObj.transform.localScale, m_Layer[m_nNowLayer].Scale, Time.deltaTime);
-                    m_Layer[m_nOldLayer].LayerObj.transform.position =
-                        Vector3.Lerp(m_Layer[m_nOldLayer].LayerObj.transform.position, m_Layer[m_nNowLayer].Pos, Time.deltaTime);
-                }
-
-                if (m_nNowLayer == 1)
-                {
-                    //奥のレイヤーをアクティブにする
-                    m_Layer[m_nNowLayer + 1].LayerObj.SetActive(true);
-
-                    //レイヤーを1つ前にする
-                    m_Layer[m_nNowLayer].LayerObj.transform.localScale =
-                        Vector3.Lerp(m_Layer[m_nNowLayer].LayerObj.transform.localScale, m_Layer[m_nOldLayer - 1].Scale, Time.deltaTime);
-                    m_Layer[m_nNowLayer].LayerObj.transform.position =
-                        Vector3.Lerp(m_Layer[m_nNowLayer].LayerObj.transform.position, m_Layer[m_nOldLayer - 1].Pos, Time.deltaTime);
-
-                    m_Layer[m_nNowLayer - 1].LayerObj.transform.localScale =
-                        Vector3.Lerp(m_Layer[m_nNowLayer - 1].LayerObj.transform.localScale, m_Layer[m_nOldLayer - 2].Scale, Time.deltaTime);
-                    m_Layer[m_nNowLayer - 1].LayerObj.transform.position =
-                        Vector3.Lerp(m_Layer[m_nNowLayer - 1].LayerObj.transform.position, m_Layer[m_nOldLayer - 2].Pos, Time.deltaTime);
-                }
-
-            }
-
-            //5秒たったら
-            if (m_fTimer > 5f)
-            {
-                m_fTimer = 0.0f;
-                m_isPush = false;
-            }
+            m_isPush = false;
         }
+ 
     }
+
+
+    private bool LayerChange()
+    {
+        //レイヤーが更新されていなかったら終了
+     //   if (m_nOldLayer == m_nNowLayer) { return; }
+
+        //サイズと位置の更新
+        for(int i = 0; i<m_Layer.Count;i++)
+        {
+            if(m_Layer[i].LayerObj == null) { continue; }
+            m_Layer[i].LayerObj.transform.localScale =
+                Vector3.Lerp(m_Layer[i].LayerObj.transform.localScale, m_Layer[i].Scale, Time.deltaTime);
+            m_Layer[i].LayerObj.transform.position =
+                Vector3.Lerp(m_Layer[i].LayerObj.transform.position, m_Layer[i].Pos, Time.deltaTime);
+        }
+
+        if(m_Layer[2].LayerObj.transform.position == m_Layer[2].Pos)
+        {
+            return true;
+        }
+
+        return false;
+      
+    }
+
 }
