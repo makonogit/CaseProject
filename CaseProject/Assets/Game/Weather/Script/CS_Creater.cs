ec1cfd1274f5b8b6ff6317db3040380966f79e48
Nakagawa <utt014257u6s02@outlook.jp>
2024-05-29 12:23:47 +0900

    Update:風の向きを正しく生成されるように更新

--- CaseProject/Assets/Game/Weather/Script/CS_Creater.cs
+++ CaseProject/Assets/Game/Weather/Script/CS_Creater.cs
@@ -39,100 +39,28 @@ public class CS_Creater : MonoBehaviour
         Vector3 dir = direction;
         dir.y = 0;
         dir.Normalize();
-
+        bool IsLeftHand =dir.x > 0;
         // 方向の設定
         Quaternion rotation = Quaternion.identity;
         // 風の生成
         GameObject obj = GameObject.Instantiate(m_objWind, pos, rotation);
+        if (IsLeftHand) obj.transform.localScale = InvertScaleX(obj);
         CS_Wind cswind = obj.GetComponent<CS_Wind>();  //風のスクリプト取得
-        cswind.WindDirection = dir.x > 0 ? CS_Wind.E_WINDDIRECTION.LEFT : CS_Wind.E_WINDDIRECTION.RIGHT; //風の向き設定　追加：菅眞心
+        cswind.WindDirection = IsLeftHand ? CS_Wind.E_WINDDIRECTION.LEFT : CS_Wind.E_WINDDIRECTION.RIGHT; //風の向き設定　追加：菅眞心
         cswind.WindPower = direction.magnitude * m_fWindPowerMagnification;
         cswind.SetCameraPos = this.transform.position;
         cswind.DeleteFlag = true;
         cswind.SetCS_Player(m_player);
     }
-
-    // 風の生成する関数（購読 subscribe）
-    // 引数：人差し指の付け根の位置
-    // 引数：進行方向
-    // 戻り値：なし
-    //
-    // ※引数、戻り値はイベントの発行側で決まる。
-    //
-    private void CreateWind(Vector3 position,Vector3 direction) 
+    // スケールXを反転する
+    // 引き数：反転したいオブジェ
+    // 戻り値：反転した値
+    private Vector3 InvertScaleX(GameObject obj) 
     {
-        Debug.Log("Create Wind");
-        
-        Vector3 dir = direction;
-        dir.y = 0;
-        dir.Normalize();
-
-        Vector3 pos =GetWindPosition(dir);
-        // 方向の設定
-        Quaternion rotation = Quaternion.EulerAngles(0, 0, 0);
-        // 風の生成
-        GameObject obj = GameObject.Instantiate(m_objWind, pos, rotation);
-        // サイズ変更
-
-        //Vector3 scale = obj.transform.localScale;
-        //scale.x = direction.magnitude * m_fWindPower * dir.x;
-        //obj.transform.localScale = scale;
-
-        //風の力から風オブジェクトの数を決定
-        int nWindObjNum = (int)((direction.magnitude * m_fWindPowerMagnification * dir.x) / 10) - 1;
-        nWindObjNum = Mathf.Abs(nWindObjNum);
-        float fWindX = pos.x;
-
-        //風オブジェクトを生成する
-        while (nWindObjNum > 0)
-        {
-            //風のサイズ分ずらして配置
-            fWindX += 4.0f * dir.x;
-            Vector3 Pos = new Vector3(fWindX, pos.y, 0.0f);
-
-            GameObject windobj = Instantiate(m_objWind, Pos, rotation);
-            CS_Wind cswind = windobj.GetComponent<CS_Wind>();  //風のスクリプト取得
-            cswind.WindDirection = dir.x > 0 ? CS_Wind.E_WINDDIRECTION.LEFT : CS_Wind.E_WINDDIRECTION.RIGHT; //風の向き設定　追加：菅眞心
-            cswind.WindPower = direction.magnitude * m_fWindPowerMagnification * dir.x;
-            cswind.SetCameraPos = this.transform.position;
-            cswind.DeleteFlag = true;
-
-            if (cswind.WindDirection == CS_Wind.E_WINDDIRECTION.LEFT)
-            {
-                windobj.transform.localScale = new Vector3(windobj.transform.localScale.x * -1, windobj.transform.localScale.y, 1.0f);
-            }
-
-            nWindObjNum--;
-
-        }
-
-        CS_Wind cs_wind = obj.GetComponent<CS_Wind>();  //風のスクリプト取得
-        cs_wind.WindDirection = dir.x > 0 ? CS_Wind.E_WINDDIRECTION.LEFT : CS_Wind.E_WINDDIRECTION.RIGHT;　//風の向き設定　追加：菅眞心
-        cs_wind.WindPower = direction.magnitude * m_fWindPowerMagnification * dir.x;
-        cs_wind.SetCameraPos = this.transform.position;
-        cs_wind.DeleteFlag = true;
-
-        //プレイヤーの移動関数を直接呼び出し
-        float windpower = direction.magnitude * m_fWindPowerMagnification * dir.x;
-        windpower = Mathf.Abs(windpower);
-
-        m_player.WindMove(cs_wind.WindDirection, windpower);
-
+        Vector3 scale = obj.transform.localScale;
+        scale.x *= -1;
+        return scale;
     }
+
     
-    // 風の生成位置を求める関数
-    // 引数：風の方向
-    // 戻り値：風の生成位置
-    private Vector3 GetWindPosition(Vector3 direction) 
-    {
-        Vector3 pos = this.transform.position;
-        pos.z = 0;// 奥行は必要ない
-        const float offsetX = 8;
-        if (direction.x < 0) pos.x += offsetX;
-        else pos.x -= offsetX;
-
-        pos.y -= 2.0f;
-
-        return pos;
-    }
 }
