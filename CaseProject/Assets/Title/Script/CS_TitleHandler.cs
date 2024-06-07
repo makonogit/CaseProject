44ba63087e9ef418c233b338f95ad54b243b163e
Nakagawa <utt014257u6s02@outlook.jp>
2024-06-07 14:21:22 +0900

    Update:上げ直し

--- CaseProject/Assets/Title/Script/CS_TitleHandler.cs
+++ CaseProject/Assets/Title/Script/CS_TitleHandler.cs
@@ -10,36 +10,11 @@ using UnityEngine;
 
 public class CS_TitleHandler : MonoBehaviour
 {
-    [SerializeField, Header("ハンドサイン")]
-    private CS_HandSigns m_handSigns;
-
-    //private List<HandLandmarkListAnnotation> m_handLandmark = new List<HandLandmarkListAnnotation>();
-
     [SerializeField, Header("次のシーンの名前")]
     private string m_nextSceneName;
-
-    private Vector3[] m_v3HandDir = new Vector3[2];    //手の向き
-    private Vector3[] m_v3HandMove = new Vector3[2];   //手の動き
-
-    [SerializeField] private List<Vector3> m_Directions = new List<Vector3>();
+    private List<Vector3> m_Directions = new List<Vector3>();
     private List<float> m_Time = new List<float>();
-    //[SerializeField, Header("両手の初期位置")]
-    //[Header("0:右手の初期位置")]
-    //[Header("1:左手の初期位置")]
-    //private GameObject[] m_startHandsObjPosition = new GameObject[2];
-    //private Vector3[] m_startHandsScreenPosition = new Vector3[2];
-
-    //[SerializeField, Header("シリウスが出てくる両手の位置")]
-    //[Header("0:右手の初期位置")]
-    //[Header("1:左手の初期位置")]
-    //private GameObject[] m_bornSeriusObjPosition = new GameObject[2];
-
-
-    //[SerializeField, Header("両手の配置の認識可能範囲")]
-    //private float m_recognizableDistance = 2.0f;
-
-    bool m_isUpdate = true;
-
+    
     //状態が待機時2の待機時間
     private float m_nowWaitTime = 0.0f;
     private float m_waitTime = 1.0f;
@@ -84,16 +59,7 @@ public class CS_TitleHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //m_handLandmark = m_handSigns.HandMark;
-        CS_HandSigns.OnCreateWinds += Swing;
-
-        //for(int i =0; i < 2; i++)
-        //{
-        //    Vector3 worldPos = m_startHandsObjPosition[i].transform.position;
-        //    m_startHandsScreenPosition[i] = worldPos;
-        //}
-
-        if (!m_handSigns) { Debug.LogWarning("CS_HandSigns.csがアタッチされていません"); }
+        CS_HandSigns.OnCreateWinds += Swing;       
     }
 
     
@@ -103,98 +69,14 @@ public class CS_TitleHandler : MonoBehaviour
     {
         CheckGoNextScene();//次のシーンへいくかどうかの処理
         
-
         // リストの更新
-        TimeOverRemoveList();
+        TimeOverRemoveList();    
         
-        //更新しないなら終了
-        if (!m_isUpdate) { return; }
-
-
-
-        //両手の情報を取得
-        for (int i = 0; i < 2; i++) 
-        {
-            //両手がパーじゃなかったら終了
-            bool is_handpose = m_handSigns.GetHandPose(m_handSigns.HandInfo[i].HandLandmark) == (byte)CS_HandSigns.HandPose.PaperSign;
-            //if (!is_handpose) { return; }
-
-            m_v3HandDir[i] = m_handSigns.GetHandDirection(m_handSigns.HandInfo[i].HandLandmark.GetLandmarkList());
-            m_v3HandMove[i] = m_handSigns.GetHandMovement(m_handSigns.HandInfo[i].vec3MoveDistanceList);
-
-        }
-
-
-        switch (m_titleState)
-        {
-            case TITLE_STATE.CALL_SERIUS:
-                m_isUpdate = false;
-                m_titleState = TITLE_STATE.BORN_SERIUS;
-                break;
-        }
-
-
         // シリウスを呼ぶ
         if (IsCallSerius()) m_titleState = TITLE_STATE.BORN_SERIUS;
-
-
-        //Debug.Log("Dir0" + m_v3HandDir[0] + "Dir1" + m_v3HandDir[1]);
-        //Debug.Log("Move0" + m_v3HandMove[0] + "Move1" + m_v3HandMove[1]);
-
-        {
-            ////ハンドマークを取得
-            ////0を右手、１を左手とする
-            //m_handLandmark = m_handSigns.HandMark;
-
-            //if (m_handLandmark.Count < 2)
-            //{
-            //    return;
-            //}
-
-
-            //string[] hand = { "右手", "左手" };
-            //PointListAnnotation point1 = m_handLandmark[0].GetLandmarkList();
-            //PointListAnnotation point2 = m_handLandmark[1].GetLandmarkList();
-            ////右手が左手側にあるならhandLandmarkの中身を入れ替える
-            //if(point1[9].transform.position.x < point2[9].transform.position.x)
-            //{
-            //    HandLandmarkListAnnotation mark = m_handLandmark[0];
-            //    m_handLandmark[0] = m_handLandmark[1];
-            //    m_handLandmark[1] = mark;
-            //}
-
-            //for (int i = 0; i < 2; i++)
-            //{
-            //    PointListAnnotation point = m_handLandmark[i].GetLandmarkList();
-
-            //    float dis = Vector2.Distance(point[9].transform.position, m_startHandsScreenPosition[i]);
-
-            //    if (dis > m_recognizableDistance) { return; }
-            //}
-
-            //    switch (m_titleState)
-            //    {
-            //        case TITLE_STATE.SET_HANDS:
-            //            for (int i = 0; i < 2; i++)
-            //            {
-            //                m_startHandsObjPosition[i].transform.position = m_bornSeriusObjPosition[i].transform.position;
-            //                m_startHandsScreenPosition[i] = m_startHandsObjPosition[i].transform.position;
-            //            }
-            //            m_titleState = TITLE_STATE.CALL_SERIUS;
-            //            break;
-            //        case TITLE_STATE.CALL_SERIUS:
-            //            for (int i = 0; i < 2; i++)
-            //            {
-            //                Destroy(m_startHandsObjPosition[i]);
-            //                Destroy(m_bornSeriusObjPosition[i]);
-            //            }
-            //            m_isUpdate = false;
-            //            m_titleState = TITLE_STATE.BORN_SERIUS;
-            //            break;
-            //    }
-        }
+                
     }
-
+    // OnDestroy is called this object is Destroyed
     private void OnDestroy(){
         CS_HandSigns.OnCreateWinds -= Swing;
     }
